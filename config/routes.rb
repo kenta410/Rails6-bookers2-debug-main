32c2e52036e1300aeb803e6d850468f784a80743
EC2 Default User <ec2-user@ip-172-31-91-134.ec2.internal>
2022-06-05 01:18:48 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get "home/about" => "homes#about", as:"about"
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resource :book_comments, only: [:create]
+  end
   resources :users
 end
